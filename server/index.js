3347e3eb6fb45e6d94c84695e8b53e38ba9a8458
Aabraham-Farias <abbraham_13905@hotmail.com>
2020-05-21 23:19:10 -0600

    Archivo principal server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const server = express();
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const PORT = process.env.PORT || 4000;
+const { errors } = require('celebrate');
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+}));
+server.use(cors());
+
+// Endpoints
+server.get('/', (req, res) => res.send('Hello World!'));
+
+server.use('/api/v1', require('../router'));
+
+server.use(errors());
+
+// exportar server para poder requerirlo desde otros archivos
+module.exports = { server, PORT };
