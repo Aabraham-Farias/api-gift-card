b3feeae7c1cef4fa04f7d181350ba8a416f40e81
Aabraham-Farias <abbraham_13905@hotmail.com>
2020-05-21 23:13:04 -0600

    index principal de los controladores

---
+++ controller/index.js
@@ -0,0 +1,11 @@
+const UsersController = require('./UsersController');
+const RolesController = require('./RolesController');
+const ProductsController = require('./ProductsController');
+const AddtocartController = require('./AddtocartController');
+
+module.exports = {
+    UsersController,
+    RolesController,
+    ProductsController,
+    AddtocartController,
+}
