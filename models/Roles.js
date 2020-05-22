10b0cc3c7066f700fa82fd3f5d986c1608f2c4c0
Aabraham-Farias <abbraham_13905@hotmail.com>
2020-05-21 23:16:40 -0600

    archivos de configuracion de los schemas

---
+++ models/Roles.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const rolesSchema = new Schema({
+    title: {
+        type: String,
+    },
+    is_active: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+const Roles = mongoose.model('Roles', rolesSchema);
+
+module.exports = { Roles, rolesSchema };
