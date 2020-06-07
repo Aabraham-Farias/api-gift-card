b097e87bded7f566384c371e437075edea00b11e
Aabraham-Farias <abbraham_13905@hotmail.com>
2020-06-07 15:48:32 -0600

    arreglando problemas

--- services/ProductsSerivce.js
+++ services/ProductsSerivce.js
@@ -1,19 +1,6 @@
 const { Products } = require('../models/Products');
 
 module.exports = {
-<<<<<<< HEAD
-  create: (body) => {
-    const newProduct = new Products(body);
-    return newProduct.save();
-  },
-  find: () => Products.find(),
-  findById: (id) => Products.findById(id),
-  update: (product, body) => {
-    Object.assign(product, body);
-    return product.save();
-  },
-};
-=======
     create: (body) => {
         const newProduct = new Products(body);
         return newProduct.save();
@@ -24,5 +11,4 @@ module.exports = {
         Object.assign(product, body);
         return product.save();
     },
-}
->>>>>>> c7370140e49c537908e401e4c3f3ee0a7c265295
+};
